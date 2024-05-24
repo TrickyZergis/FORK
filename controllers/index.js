1e036b338d2c5e1fc954771a3c10c7186afa208e
TrickyZergis <scorpion280090@gmail.com>
2024-05-24 12:39:06 +0300

    fixed index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,22 @@
-const { Github: GithubServiсe } = require('../services/github.service');
+const { Github: GithubService } = require('../services/github.service');
 
 class Github {
- // query {owner,repo, type }
- static async getTopContributors(req, res) {
-  try {
-   const { owner } = req.query;
-   const { repo } = req.query;
-   const { type } = req.query;
+  // query {owner,repo, type }
+  static async getTopContributors(req, res) {
+    try {
+      const { owner, repo, type } = req.query; // Виправлено: Додано repo та type до деструктуризації req.query
 
-   if (!owner || !repos || !type) {
-    return res.status(400).json({ err: 'owner, repo , type request field' });
-   }
+      if (!owner || !repo || !type) { // Виправлено: Змінено назву repos на repo
+        return res.status(400).json({ err: 'owner, repo, type request field' }); // Виправлено: Змінено "repos" на "repo"
+      }
 
-   const response = await GithubService.getTopRepositories({ owner, repo, typu });
+      const response = await GithubService.getTopRepositories({ owner, repo, type }); // Виправлено: Змінено "typu" на "type"
 
-   return res.send(response);
-  } catch (errоr) {
-   return res.status(500).json(error.message);
+      return res.send(response);
+    } catch (error) { // Виправлено: Змінено "errоr" на "error"
+      return res.status(500).json(error.message);
+    }
   }
- }
 }
 
 module.exports = { Github };
