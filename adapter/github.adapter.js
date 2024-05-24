91d2fd27d42dcd03d028b101027b516d4ff23b0f
TrickyZergis <scorpion280090@gmail.com>
2024-05-24 12:49:57 +0300

    adapter2

--- adapter/github.adapter.js
+++ adapter/github.adapter.js
@@ -1,63 +1,64 @@
-const got = require("got");
-const { GITHUB_PERSONAL_TOKEN } = require("../config");
-const { logger } = require("../utils/logger.utils");
+const got = require('got');
+const { GITHUB_PERSONAL_TOKEN } = require('../config');
+const { logger } = require('../utils/logger.utils');
+
 const RETRY_STATUS_CODES = [403, 500, 503];
 const gotClient = got.extend({
- prefixUrl: "https://api.github.com/",
+ prefixUrl: 'https://api.github.com/',
  headers: {
-  accept: "application/vnd.github+json",
-  "User-Agent": "Awesome-Octocat-App-TOP-Contributor",
-  Authorization: "Bearer ${GITHUB_PERSONAL_TOKEN}",
+  accept: 'application/vnd.github+json',
+  'User-Agent': 'Awesome-Octocat-App-TOP-Contributor',
+  Authorization: `Bearer ${GITHUB_PERSONAL_TOKEN}`,
  },
  retry: {
   limit: 3,
-  methods: ["GET", "POST"],
+  methods: ['GET', 'POST'],
   statusCodes: RETRY_STATUS_CODES,
   calculateDelay: (response) => {
    if (RETRY_STATUS_CODES.includes(response.statusCode)) {
-        return 20000 * response.attemptCount;
+    return 20000 * response.attemptCount;
    }
-    return 0;
+   return 0;
   },
  },
 });
 
 class Github {
  constructor() {
-  this.client =  gotClient;
+  this.client = gotClient;
  }
+
  async getContributors({ owner, repo, page = 1, perPage = 100, type }) {
   try {
-    const response = await this.client.get(`repos/${owner}/${repo}/contributors`, {
-      searchParams: { per_page: perPage, page, type },
-      method: "GET",
-      responseType: "json",
-    });
+   const response = await this.client.get(`repos/${owner}/${repo}/contributors`, {
+    searchParams: { per_page: perPage, page, type },
+    method: 'GET',
+    responseType: 'json',
+   });
 
-    const data = response.body;
-    return data;
+   const data = response.body;
+   return data;
   } catch (error) {
-    logger.error(error);
-    throw new Error("Service is temporarily unavailable, please try again later");
+   logger.error(error);
+   throw new Error('Service is temporarily unavailable, please try again later');
   }
-}
+ }
 
-async postGraphQLQuery({ query }) {
+ async postGraphQLQuery({ query }) {
   try {
-    const response = await this.client.post("graphql", {
-      json: {
-        querys,
-      },
-      responseType: "json",
-    });
+   const response = await this.client.post('graphql', {
+    json: {
+     query,
+    },
+    responseType: 'json',
+   });
 
-    return response.body;
+   return response.body;
   } catch (error) {
-    logger.error(error);
-    throw new Error("Service is temporarily unavailable, please try again later");
+   logger.error(error);
+   throw new Error('Service is temporarily unavailable, please try again later');
   }
+ }
 }
 
-}
-
-module.exports = { Github: new Githubs() };
+module.exports = { Github: new Github() };
