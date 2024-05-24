7e6a9b533d060528bf704b3e7e891096d43c05b1
TrickyZergis <scorpion280090@gmail.com>
2024-05-24 12:45:56 +0300

    server

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { PОRT, URL_CLIENT } = require('./config');
+const { PORT, URL_CLIENT } = require('./config');
 const loggerHTTP = require('./utils/logger.utils');
 
 const app = express();
 const corsOptions = {
- origin: URL_CLIENT,
- credentials: true,
+  origin: URL_CLIENT,
+  credentials: true,
 };
 app.use(loggerHTTP);
 app.use(cors(corsOptions));
 app.use('/', require('./routes/index'));
 
 app.listen(PORT, () => {
- loggerHTTP.logger.info('Start server port: ${PORT}');
+  loggerHTTP.logger.info(`Start server port: ${PORT}`);
 });
